77b83a4fa8d574d496a9082f99f8b8e160eec069
Daniel Oliveira <dan.da.cruz.oliveira@gmail.com>
2016-12-16 15:19:35 -0500

    added socket io code. Don't know if it works yet.

--- client/js/script.js
+++ client/js/script.js
@@ -1,14 +1,25 @@
 angular.module('ChatApp', []).controller('ChatController', function() {
   var chat = this;
+  var socket = new io.Socket();
+  socket.connect('http://127.0.0.1:8080'); 
 
   chat.text = "";
 
-  chat.messages = [
-    {class:'received', text:'Hi!'},
-    {class:'sent', text:'Hi Dan'}
-  ];
+  chat.users = Array();
+
+  chat.messages = Array();
 
   chat.SendMessage = function(){
-    console.log(chat.text);
+    socket.emit('message', chat.text);
+    chat.messages.push({class:'sent', text:msg});
   };
+
+  socket.on('message', function(msg){
+    chat.messages.push({class:'received', text:msg});
+  });
+
+  socket.on('users', function(users){
+    chat.users = users;
+  });
+
 });
--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var generateName = require('sillyname');
 
-app.get('/api', function(req, res){
- res.send('<h1>Hello World</h1>');
+var users = Array();
+
+io.on('connection', function(socket){
+
+	//Create name and send client its name
+	var username = generateName();
+	users[socket.id] = username;
+	socket.broadcast.emit('message', "user " + users[socket.id] + " has entered the chat");
+	socket.emit('users', users);
+
+	socket.on('disconnect', function(){
+		socket.broadcast.emit('message', "user " + users[socket.id] + " has left the chat");
+	});
+
+	socket.on('message', function(msg){
+		socket.broadcast.emit('message', users[socket.id] + ": " + msg);
+	});
 });
 
 http.listen(8080, function(){
- console.log('listening on *:8080');
+	console.log('listening on *:8080');
 });
